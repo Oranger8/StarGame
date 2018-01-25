6e09af464596e6787083ebbbb3442a3c57b5b2ca
Oranger8 <oranger.belov@gmail.com>
2018-01-25 20:03:44 +0300

    Homework 1

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -4,16 +4,26 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture img1, img2, wing;
+	TextureRegion tie;
+	int stateTime, point1, point2;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img1 = new Texture("forest.jpg");
+        img2 = new Texture("forest.jpg");
+        wing = new Texture("X-Wing_Top_View.png");
+        tie = new TextureRegion(new Texture("8rlMkn-TopView.png"));
+		stateTime = 1024;
+		point1 = 0;
+		point2 = 1024;
 	}
 
 	@Override
@@ -21,13 +31,23 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.setColor(0.5f, 0.5f, 0.5f, 1);
+		batch.draw(img1, 0, point1);
+		batch.draw(img2, 0, point2);
+		batch.setColor(1, 1, 1, 1);
+		batch.draw(wing, 300, 10, 92, 100);
+		batch.draw(tie, 300, 400, 0, 0, 92, 92, 1, 1, 180);
 		batch.end();
+		if (point1 == -1024) point1 = 1024;
+		if (point2 == -1024) point2 = 1024;
+		point1--;
+		point2--;
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		img1.dispose();
+        img2.dispose();
 	}
 }
